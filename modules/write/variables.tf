bf42dd926eefde3e953b983eaa055e7826ffc351
Oleg <oleg.dragoi228@gmail.com>
2023-05-18 14:55:53 +0200

    Rewrote the variables file

--- modules/write/variables.tf
+++ modules/write/variables.tf
@@ -1,29 +1,24 @@
 variable "answer_1" {
   description = "Answer to the question nr. 1"
   type        = string
-  default     = var.an_1
 }
 
 variable "answer_2" {
   description = "Answer to the question nr. 1"
   type        = string
-  default     = var.an_2
 }
 
 variable "answer_3" {
   description = "Answer to the question nr. 1"
   type        = string
-  default     = var.an_3
 }
 
 variable "answer_4" {
   description = "Answer to the question nr. 1"
   type        = string
-  default     = var.an_4
 }
 
 variable "answer_5" {
   description = "Answer to the question nr. 1"
   type        = string
-  default     = var.an_5
 }
