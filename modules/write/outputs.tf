673f7fa61f9b64c26d8e11525557fd3d3f493c51
Oleg <oleg.dragoi228@gmail.com>
2023-05-18 14:45:20 +0200

    Task 3

---
+++ modules/write/outputs.tf
@@ -0,0 +1,19 @@
+output "out-answer_1" {
+  value = var.answer_1
+}
+
+output "out-answer_2" {
+  value = var.answer_2
+}
+
+output "out-answer_3" {
+  value = var.answer_3
+}
+
+output "out-answer_4" {
+  value = var.answer_4
+}
+
+output "out-answer_5" {
+  value = var.answer_5
+}
