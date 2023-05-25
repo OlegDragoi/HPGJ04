836ed4562940cad7e8ef555459fd0ea826f923f5
Oleg <oleg.dragoi228@gmail.com>
2023-05-25 15:01:13 +0200

    Revert "Deleted variables from main folder"
    This reverts commit 8cf39c71ec9d033150a850c4ec5fd4e395b9a3a0.

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "answer_1" {
+  description = "Answer to the question nr. 1"
+  type        = string
+}
+
+variable "answer_2" {
+  description = "Answer to the question nr. 1"
+  type        = string
+}
+
+variable "answer_3" {
+  description = "Answer to the question nr. 1"
+  type        = string
+}
+
+variable "answer_4" {
+  description = "Answer to the question nr. 1"
+  type        = string
+}
+
+variable "answer_5" {
+  description = "Answer to the question nr. 1"
+  type        = string
+}
