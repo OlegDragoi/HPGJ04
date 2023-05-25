f7b2a373214321ffed67fa3e38961086bb54409d
Oleg <oleg.dragoi228@gmail.com>
2023-05-25 14:28:48 +0200

    added arguments

--- main.tf
+++ main.tf
@@ -11,7 +11,12 @@ module "HPGJ04-read" {
 }
 
 module "HPGJ04-write" {
-  source  = "./modules/write"
-  content = "test"
-  name    = "Task 3"
+  source   = "./modules/write"
+  content  = "test"
+  name     = "Task 3"
+  answer_1 = var.answer_1
+  answer_2 = var.answer_2
+  answer_3 = var.answer_3
+  answer_4 = var.answer_4
+  answer_5 = var.answer_5
 }
