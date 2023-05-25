acbddc53ffbc3909b6cedd46aa8568e7d6864e99
Oleg <oleg.dragoi228@gmail.com>
2023-05-25 14:08:27 +0200

    idk, hope it works

--- modules/write/outputs.tf
+++ modules/write/outputs.tf
@@ -1,19 +1,19 @@
 output "out-answer_1" {
-  value = var.answer_1
+  value = "plan" #var.answer_1
 }
 
 output "out-answer_2" {
-  value = var.answer_2
+  value = "object" #var.answer_2
 }
 
 output "out-answer_3" {
-  value = var.answer_3
+  value = "while" #var.answer_3
 }
 
 output "out-answer_4" {
-  value = var.answer_4
+  value = "using meta-argument" #var.answer_4
 }
 
 output "out-answer_5" {
-  value = var.answer_5
+  value = "using the -var flag" #var.answer_5
 }
