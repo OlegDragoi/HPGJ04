d1590777e57cf7e2731638a68a4abdc49b079e25
Oleg <oleg.dragoi228@gmail.com>
2023-05-25 14:15:23 +0200

    If only I knew

--- main.tf
+++ main.tf
@@ -15,3 +15,28 @@ module "HPGJ04-write" {
   content = "test"
   name    = "Task 3"
 }
+
+variable "answer_1" {
+  description = "Answer to the question nr. 1"
+  type        = string
+}
+
+variable "answer_2" {
+  description = "Answer to the question nr. 2"
+  type        = string
+}
+
+variable "answer_3" {
+  description = "Answer to the question nr. 3"
+  type        = string
+}
+
+variable "answer_4" {
+  description = "Answer to the question nr. 4"
+  type        = string
+}
+
+variable "answer_5" {
+  description = "Answer to the question nr. 5"
+  type        = string
+}
